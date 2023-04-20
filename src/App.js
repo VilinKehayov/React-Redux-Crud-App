2436a668b260f9c911433d99b0eb7df07c10d873
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-04-20 10:32:09 +0300

    Added useSelector hook, made input for adding user and div for displaying users, maped all the fake data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
-import './App.css';
+import "./App.css";
+import { useSelector } from "react-redux";
 
 function App() {
+
+  const userList = useSelector((state)=> state.users.value)
   return (
     <div className="App">
-    Its vilin
+      {""}
+      <div className="addUser">
+        <input type="text" placeholder="Name..." />
+        <input type="text" placeholder="Username..." />
+        <button>Add User</button>
+      </div>
+      <div className="displayUsers">
+        {userList.map((user)=> {
+          return <h1>{user.name}</h1>
+        })}
+      </div>
     </div>
   );
 }
