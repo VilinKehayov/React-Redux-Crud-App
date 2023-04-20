34b1cfd5e146f701f52106f5dcd9574640486aed
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-04-20 11:28:57 +0300

    Added and exported deleteUser reducer, filtered the array using user id

--- src/features/Users.js
+++ src/features/Users.js
@@ -2,6 +2,7 @@ import {createSlice} from "@reduxjs/toolkit"
 import {UsersData} from "../FakeData"
 
 
+
 export const userSlice = createSlice({
     name: "users",
     initialState: {value: UsersData},
@@ -9,8 +10,12 @@ export const userSlice = createSlice({
         addUser: (state, action) => {
             state.value.push(action.payload)
         },
+
+        deleteUser: (state, action) => {
+            state.value = state.value.filter((user)=> user.id !== action.payload.id);
+        }
     },
 })
 
-export const {addUser} = userSlice.actions
+export const {addUser, deleteUser} = userSlice.actions
 export default userSlice.reducer;
