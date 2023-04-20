a7dce71ea1435082a791be43a0e5c930f6b9d7fd
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-04-20 10:54:59 +0300

    Imported usedispatch, useState, Added dispatch to the button, making component state for the inputs

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import "./App.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import {addUser} from "./features/Users.js"
+import {useState} from "react";
 
 function App() {
+  const dispatch = useDispatch();
+  const userList = useSelector((state) => state.users.value);
 
-  const userList = useSelector((state)=> state.users.value)
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
   return (
     <div className="App">
       {""}
       <div className="addUser">
         <input type="text" placeholder="Name..." />
         <input type="text" placeholder="Username..." />
-        <button>Add User</button>
+        <button onClick={()=>{dispatch(addUser({id:0, name:"", username:""}))}}>Add User</button>
       </div>
       <div className="displayUsers">
-        {userList.map((user)=> {
-          return <h1>{user.name}</h1>
+        {userList.map((user) => {
+          return (
+            <div>
+              <h1>{user.name}</h1>
+              <h1>{user.username}</h1>
+            </div>
+          );
         })}
       </div>
     </div>
